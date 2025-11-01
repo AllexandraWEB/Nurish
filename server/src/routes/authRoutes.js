31cf3f3577a14edc9e028c844473a515ddd94679
Alexandra Tsimentarova <cimentarowa@gmail.com>
2025-11-01 23:53:10 +0200

    fix: Render deploy issues

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import { handleRegister, handleLogin } from '../controllers/authController.js';
+import express from "express";
+import { handleRegister, handleLogin } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post('/register', handleRegister);
-router.post('/login', handleLogin);
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+router.post("/register", handleRegister);
+router.post("/login", handleLogin);
 
 export default router;
-
-
